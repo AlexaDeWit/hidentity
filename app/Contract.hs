5a6d508bc110894ce794adb1c5bd50924b466fe0
Alexandra DeWit <alexa.dewit@gmail.com>
2018-06-19 19:10:38 +0200

    Finish scaffolding out types

--- app/Contract.hs
+++ app/Contract.hs
@@ -7,12 +7,13 @@ module Contract
   , symmetricalChannel
   ) where
 
-import Protolude               (Maybe, panic, ($))
-import Data.ByteString.Lazy    (ByteString)
+import Protolude               (($), Either(..), IO, panic, return)
+import Data.ByteString         (ByteString)
 import Jose.Jwk                (Jwk)
 import Data.Text               (pack)
 import Jose.Jwa                (JweAlg, Enc, JwsAlg)
-
+import Jose.Jwe                (jwkDecode)
+import Jose.Jwt                (JwtContent(..), JwtError(..), JwtClaims(..))
 
 class Keyable a where
   retrieveKey :: a -> Jwk
@@ -33,22 +34,37 @@ data (Keyable e1, Keyable e2) => Channel e1 e2
 
 data PayloadFormat
   = WrappedJwt JwsAlg JweAlg Enc
-  | PlainJwt JwsAlg
 
 
 data Direction
   = Incoming
   | Outgoing
 
+data PayloadError
+  = JwtErr JwtError
+  | IncorrectFormatError
 
 symmetricalChannel :: (Keyable sender, Keyable recipient) => sender -> recipient -> PayloadFormat -> Channel sender recipient
 symmetricalChannel s r form = Channel sendingContract receivingContract where
     sendingContract   = Contract form s r
     receivingContract = Contract form r s
 
-retrievePayload :: (Keyable s, Keyable r) => Channel s r -> Direction -> ByteString -> Maybe ByteString
-retrievePayload channel Incoming = applyContract $ incoming channel
-retrievePayload channel Outgoing = applyContract $ outgoing channel
+retrievePayload :: (Keyable s, Keyable r) => Channel s r -> Direction -> ByteString -> IO (Either PayloadError JwtClaims)
+retrievePayload channel direction =
+  let
+    unpack :: (Keyable s, Keyable r) => Contract s r -> ByteString -> IO (Either PayloadError JwtClaims)
+    unpack contract input = case payloadFormat contract of
+      WrappedJwt jwsAlg jweAlg enc -> do
+        let recipientKey = retrieveKey $ recipient contract
+        result <- jwkDecode recipientKey input
+        case result of
+          Right (Unsecured content) -> decodeAndUnpackJwt (retrieveKey $ sender contract) jwsAlg content
+          Right _                   -> return $ Left IncorrectFormatError
+          Left err                  -> return $ Left $ JwtErr err
+  in
+  case direction of
+    Incoming -> unpack $ incoming channel
+    Outgoing -> unpack $ outgoing channel
 
-applyContract :: (Keyable s, Keyable r) => Contract s r -> ByteString -> Maybe ByteString
-applyContract = panic $ pack "Thus I die. Thus, thus, thus. Now I am dead"
+decodeAndUnpackJwt :: Jwk -> JwsAlg -> ByteString -> IO (Either PayloadError JwtClaims)
+decodeAndUnpackJwt = panic $ pack $ "Typecheck"
