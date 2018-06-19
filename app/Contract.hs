07f0ffeaa60d41aa58e8f0689b88a1f174f1c4df
Alexandra DeWit <alexa.dewit@gmail.com>
2018-06-19 17:36:43 +0200

    Start defining the communication contract functions

--- app/Contract.hs
+++ app/Contract.hs
@@ -3,18 +3,23 @@ module Contract
   , Channel(..)
   , JweContract(..)
   , Keyable(..)
+  , Direction(..)
   , symmetricalChannel
   ) where
 
-import Protolude (Maybe)
-import Jose.Jwk  (Jwk)
-import Jose.Jwa  (JweAlg, Enc, JwsAlg)
+import Protolude               (Maybe, panic, ($))
+import Data.ByteString.Lazy    (ByteString)
+import Jose.Jwk                (Jwk)
+import Data.Text               (pack)
+import Jose.Jwa                (JweAlg, Enc, JwsAlg)
 
-data JweContract
-  = JweContract JweAlg Enc
 
 class Keyable a where
   retrieveKey :: a -> Jwk
+
+
+data JweContract
+  = JweContract JweAlg Enc
 
 data (Keyable s, Keyable r) => Contract s r
   = Contract
@@ -25,13 +30,24 @@ data (Keyable s, Keyable r) => Contract s r
   }
 
 data (Keyable e1, Keyable e2) => Channel e1 e2
- = Channel
-  { outGoing :: Contract e1 e2
-  , inComing :: Contract e2 e1
+  = Channel
+  { outgoing :: Contract e1 e2
+  , incoming :: Contract e2 e1
   }
+
+data Direction
+  = Incoming
+  | Outgoing
+
 
 symmetricalChannel :: (Keyable sender, Keyable recipient) => sender -> recipient -> JwsAlg -> Maybe JweContract -> Channel sender recipient
 symmetricalChannel s r alg jwec = Channel sendingContract receivingContract where
     sendingContract   = Contract alg jwec s r
     receivingContract = Contract alg jwec r s
 
+retrievePayload :: (Keyable s, Keyable r) => Channel s r -> Direction -> ByteString -> Maybe ByteString
+retrievePayload channel Incoming = applyContract $ incoming channel
+retrievePayload channel Outgoing = applyContract $ outgoing channel
+
+applyContract :: (Keyable s, Keyable r) => Contract s r -> ByteString -> Maybe ByteString
+applyContract = panic $ pack "Thus I die. Thus, thus, thus. Now I am dead"
