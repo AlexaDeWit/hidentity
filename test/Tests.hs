fd6ed6f5705e3b7d3dd190d15753cf44cce792a8
Alexandra DeWit <alexa.dewit@gmail.com>
2018-06-19 17:22:18 +0200

    Minimalist test stuff up and running

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,6 +1,9 @@
 module Main where
 
 import Protolude
+import Test.Hspec
 
 main :: IO ()
-main = putStrLn "Yo"
+main = hspec $ do
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (Just 23 :: Maybe Int)
